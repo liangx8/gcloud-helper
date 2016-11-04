ea6400309de0b9a20bc75288362d256842e35103
wang gang on notebook <wang.notebook.gang@gmail.com>
2016-11-05 06:17:05 +0800

    update

--- gcs/bucket.go
+++ gcs/bucket.go
@@ -4,6 +4,7 @@ import (
 
 	"golang.org/x/net/context"
 	"cloud.google.com/go/storage"
+	"google.golang.org/api/iterator"
 )
 
 type (
@@ -18,108 +19,41 @@ type (
 //   usage:
 //   err := All(ctx,bucket,gcs.ObjectCallback(cb))
 //   err := All(ctx,bucket,gcs.AttrCallback(cb))
-func (bucket *Bucket)All(cb interface{}) error {
-	var ea func(string,interface{}) error
-	ea = func(prefix string,cb1 interface{}) error{
-		query := &storage.Query{Prefix:prefix,Delimiter:"/"}
-		var pf []string
-//		for query != nil {
-		objs,err := bucket.B.List(bucket.C,query)
-		if err != nil{ return err}
-		pf=objs.Prefixes
-		switch cb2 := cb1.(type){
-		case ObjectCallback:
-			for _,res := range objs.Results {
-				err = cb2(bucket.B.Object(res.Name))
-				if err != nil { return err }
-			}
-		case AttrCallback:
-			for _,res := range objs.Results {
-				err = cb2(res)
-				if err != nil { return err }
-			}
-		default:
-			return fmt.Errorf("Error: Unsupport callback type %T", cb2 )
+func (bucket *Bucket)Objects(cb interface{},query *storage.Query) error {
+	itr := bucket.B.Objects(bucket.C,query)
+	for{
+		objAttrs,err := itr.Next()
+		if err == iterator.Done {
+			break;
 		}
-
-		// not sure what is objs.Next, studying it in future
-//			query = objs.Next
-//		}
-		for _,p:=range pf {
-			err := ea(p,cb1)
-			if err != nil { return err }
-		}
-		return nil
-	}
-	return ea("",cb)
-}
-
-func (bucket *Bucket)List(prefix string,cb func([]string,[]*storage.ObjectAttrs)error)error{
-	query := &storage.Query{Prefix:prefix,Delimiter:"/"}
-	objs,err := bucket.B.List(bucket.C,query)
-	if err != nil { return err}
-	if len(objs.Prefixes)==0 && len(objs.Results)==0 {
-		return storage.ErrObjectNotExist
-	}
-	return cb(objs.Prefixes,objs.Results)
-}
-// if name of object exists, it will be overwrite
-// guessMimeType guess mime type according the filename
-func (bucket *Bucket)NewObjectWriter(name string,guessMimeType func(string)string,f func(*storage.Writer)error) error{
-	obj := bucket.B.Object(name)
-	w := obj.NewWriter(bucket.C)
-
-	if err := f(w); err != nil {
-		return err
-	}
-	if err := w.Close(); err != nil {
-		return err
-	}
-	if guessMimeType != nil {
-		if _,err :=obj.Update(bucket.C,storage.ObjectAttrs{ContentType:guessMimeType(name)}); err != nil {
+		if err != nil {
 			return err
 		}
+		switch cb1 :=cb.(type){
+		case ObjectCallback:
+		case AttrCallback:
+			err = cb1(objAttrs)
+			if err != nil { return err }
+		default:
+			return fmt.Errorf("Error: Unspupport callback type %T",cb)
+		}
 	}
 	return nil
 }
-func (bucket *Bucket)NewObjectReader(name string, f func(*storage.Reader)error)error{
-	
-	r,err:=bucket.B.Object(name).NewReader(bucket.C)
-	if err != nil { return err }
-	defer r.Close()
-	err = f(r)
-	if err != nil { return err }
-
-	return nil
-}
-
-func (bucket *Bucket)Delete(name string,fb func(string))error{
-	err := bucket.B.Object(name).Delete(bucket.C)
-	if err != nil {
-		// name is perhps prefix
-		return bucket.DeletePrefix(name,fb)
+func AllBucket(ctx context.Context,
+	client *storage.Client,
+	projecdtID string,
+	cb func(bkt *storage.BucketAttrs) error) error{
+	itr := client.Buckets(ctx,projecdtID)
+	for {
+		bktAttrs,err := itr.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {	return err }
+		err = cb(bktAttrs)
+		if err != nil { return err }
 	}
-	fb(name)
 	return nil
 }
-func (bucket *Bucket)DeletePrefix(prefix string,fb func(string)) error{
-	
-	return bucket.List(prefix,func(prefixes []string, attrs []*storage.ObjectAttrs)error{
 
-		for _,p:=range prefixes{
-			err := bucket.DeletePrefix(p,fb)
-			if err != nil {
-				return err
-			}
-		}
-		for _,attr := range attrs{
-			err := bucket.B.Object(attr.Name).Delete(bucket.C)
-			if err != nil {
-				return err
-			}
-
-			fb(attr.Name)
-		}
-		return nil
-	})
-}
